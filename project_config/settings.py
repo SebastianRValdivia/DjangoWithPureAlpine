be31c6ba74946f99f962af5033a9a6304a6d01d7
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-05-10 18:15:35 -0300

    basic _base.html index page

--- project_config/settings.py
+++ project_config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project_config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'global_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
